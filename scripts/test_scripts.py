829fb181ede0ea7b11e05d3da118210c36f6d99e
jsamuelsen <jsamuelsen11@gmail.com>
2026-02-16 19:23:40 -0500

    chore: updated test runner to set kong enterprise image

--- scripts/test_scripts.py
+++ scripts/test_scripts.py
@@ -1,64 +1,74 @@
+import os
 import subprocess
 import sys
 
+__BASE_CMD = [
+    sys.executable,
+    "-m",
+    "pytest",
+]
+__DEFAULT_KONG_TEST_IMAGE = ("KONG_TEST_IMAGE", "kong/kong-gateway:latest")
+__E2E_TESTS = "./tests/e2e/"
+__INTEGRATION_TESTS = "./tests/integration/"
+__UNIT_TESTS = "./tests/unit/"
 
-def _run_process(cmd: list[str]) -> None:
+
+def __get_kong_env() -> dict[str, str]:
+    """Get environment variables for Kong tests."""
+    env = os.environ.copy()
+    env.setdefault(*__DEFAULT_KONG_TEST_IMAGE)
+    return env
+
+
+def __run_process(cmd: list[str], env: dict[str, str] | None = None) -> None:
     """Run a process with additional arguments."""
     # Allow passing additional arguments to pytest
     # sys.argv[0] is the script name
     extra_args = sys.argv[1:]
 
     try:
-        subprocess.check_call(cmd + extra_args)
+        subprocess.check_call(cmd + extra_args, env=env)
     except subprocess.CalledProcessError as e:
         sys.exit(e.returncode)
 
 
 def coverage() -> None:
     """Run unit tests with coverage report."""
-    base_cmd = [
-        sys.executable,
-        "-m",
-        "pytest",
+    cmd = [
+        *__BASE_CMD,
         "--cov=system_operations_manager",
         "--cov-report=term-missing",
-        "./tests/unit/",
+        __UNIT_TESTS,
     ]
 
-    _run_process(base_cmd)
+    __run_process(cmd)
 
 
 def e2e() -> None:
     """Run end-to-end tests."""
-    base_cmd = [
-        sys.executable,
-        "-m",
-        "pytest",
-        "./tests/e2e/",
+    cmd = [
+        *__BASE_CMD,
+        __E2E_TESTS,
     ]
 
-    _run_process(base_cmd)
+    __run_process(cmd, env=__get_kong_env())
 
 
 def integration() -> None:
     """Run integration tests."""
-    base_cmd = [
-        sys.executable,
-        "-m",
-        "pytest",
-        "./tests/integration/",
+    cmd = [
+        *__BASE_CMD,
+        __INTEGRATION_TESTS,
     ]
 
-    _run_process(base_cmd)
+    __run_process(cmd, env=__get_kong_env())
 
 
 def unit() -> None:
     """Run unit tests."""
-    base_cmd = [
-        sys.executable,
-        "-m",
-        "pytest",
-        "./tests/unit/",
+    cmd = [
+        *__BASE_CMD,
+        __UNIT_TESTS,
     ]
 
-    _run_process(base_cmd)
+    __run_process(cmd)
